786189f03963d027b22025ff306ea51549144761
Nathan <nathansmota@gmail.com>
2024-04-12 17:55:31 -0300

    feat: adding router delete and put

--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getConnectedClient } = require("../database/db");
-//const { ObjectId } = require("mongodb");
+const { ObjectId } = require("mongodb");
 
 const getCollection = async () => {
     const client = await getConnectedClient();
@@ -29,6 +29,28 @@ router.post("/todos", async (req, res) => {
 
     res.status(201).json({ todo, status: false, id: newTodo.insertedId });
 
+});
+
+router.delete("/todos/:id", async (req, res) => {
+    const collection = await getCollection();
+    const id = new ObjectId(req.params.id);
+
+    const deleteTodo = await collection.deleteOne({ _id: id });
+
+    res.status(200).json(deleteTodo);
+});
+
+router.put("/todos/:id", async (req, res) => {
+    const collection = await getCollection();
+    const id = new ObjectId(req.params.id);
+    const { status } = req.body;
+    if (typeof status !== "boolean") {
+        return res.status(400).json({ message: "Bad Request. 'status' must be a boolean." });
+    }
+
+    const updateTodo = await collection.updateOne({ _id: id }, { $set: { status: !status } });
+
+    res.status(200).json(updateTodo);
 });
 
 module.exports = router;
